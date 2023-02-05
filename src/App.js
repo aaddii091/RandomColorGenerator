f36092ffe587b76cd818b10b5974fe97e0560ce2
Aditya Saxena <aaddii091@gmail.com>
2023-02-05 20:56:38 +0530

    random color generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./Header";
+const purple = "#0CF576";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function generateRandomColor() {
+  let maxVal = 0xffffff; // 16777215
+  let randomNumber = Math.random() * maxVal;
+  randomNumber = Math.floor(randomNumber);
+  randomNumber = randomNumber.toString(16);
+  let randColor = randomNumber.padStart(6, 0);
+
+  return `#${randColor.toUpperCase()}`;
 }
+const App = () => {
+  const [number, setNumber] = useState(0);
+  const [cr, setcr] = useState(purple);
+  const chg = () => {
+    setcr(generateRandomColor());
+    console.log("hi");
+    setNumber(number + 1);
+  };
 
+  document.body.style.backgroundColor = cr;
+  return (
+    <>
+      <Header />
+      <div className="center">
+        <h2>Generate Random Color</h2>
+        <h1>{cr}</h1>
+        <div className="container">
+          <button onClick={chg}>Click Here !</button>
+        </div>
+      </div>
+    </>
+  );
+};
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Css from "./index.css";
+
+import IMG1 from "./components/diamond .png";
+const Header = () => {
+  const hamburger = document.querySelector(".hamb");
+  const navbar = document.querySelector(".ok");
+  console.log(navbar);
+  const sidenav = () => {
+    navbar.classList.add("active");
+  };
+  const cross = () => {
+    navbar.classList.remove("active");
+  };
+  return (
+    <>
+      <section class="header">
+        <div class="navbar">
+          <div class="logo">
+            <img src={IMG1} alt="" />
+            <h4>Spacify</h4>
+          </div>
+          <div class="nav">
+            <ul id="ok" class="ok">
+              <i
+                class="fa-solid fa-xmark crosss"
+                id="cross1"
+                onclick={cross}
+              ></i>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Portfolio</li>
+              <li>Contact Us</li>
+            </ul>
+            <i class="fa-solid fa-bars hamb" id="hamb" onclick={sidenav}></i>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Header;
